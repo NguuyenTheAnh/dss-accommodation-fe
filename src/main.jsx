580d062844fbe602a014f5cbed9c942ecc0c022d
The Anh Nguyen <anhnguyenthe29112004@gmail.com>
2025-12-02 23:48:01 +0700

    init dss fe

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/global.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      // {
+      //   path: "user",
+      //   element: <UserPage />
+      // },
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
